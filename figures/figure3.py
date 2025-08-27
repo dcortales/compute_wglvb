68117ee4d0adaa2de48885fd47712134ff82cd66
Diego Cortes-Morales <47925616+dcortales@users.noreply.github.com>
2025-08-27 15:43:54 +0200

    Update and rename 3_OGCM_perfect_model_test.py to figure3.py

--- figures/figure3.py
+++ figures/figure3.py
@@ -1,3 +1,4 @@
+# Figure 3: OGCM perfect model test
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib as mpl
@@ -65,8 +66,6 @@ cont_err_rel        = [0,10,30,50,70,90,110,200000]
 bounds_rel          = [0,10,30,50,70,90,110]
 boundticks_rel      = [0,10,30,50,70,90,110]
 norm_rel            = mpl.colors.BoundaryNorm(bounds_rel, newcmp_rel.N)
-
-# Figure 3: OGCM perfect model test
 
 k = 20 # sigma 26
 proj = ccrs.Robinson(central_longitude=-60)
@@ -219,81 +218,3 @@ for spine in ax0.spines.values():
     spine.set_linewidth(2)
 
 plt.savefig('figure3_VF.png', bbox_inches='tight', dpi=300)
-
-# Figure 9: OGCM comparison with Ekman pumping
-
-proj         = ccrs.Robinson(central_longitude=-60)
-BrBGcentre   = plt.get_cmap('RdYlBu_r')
-boundstick   = np.arange(-1, 1.2, 0.2)
-bounds       = np.arange(-1, 1.05, 0.1)
-norm         = mpl.colors.BoundaryNorm(bounds, BrBGcentre.N)
-titles = [
-    r'(a) R($w_{Ek}$, $w_{tot}$) at $\sigma$ = 26 kg m$^{{-3}}$',
-    r'(b) R($w_{{tot}}$, $w_{{Ek}}$) − R($w_{{tot}}$, $w_g$) at $\sigma$ = 26 kg m$^{{-3}}$',
-    r'(c) R($w_{{tot}}$, $w_g$) at 100m',
-    r'(d) R($w_{{Ek}}$, $w_{{tot}}$) at 100m'
-]
-
-# (a) R (Ekman vs w) sigma26
-file = sio.loadmat('.../OCCITENS/rho_wek_w_occitens_glob_025_annual_isolevm_filtr.mat')
-lon, lat, rho_a, isopl = file['LONimone'], file['LATimone'], file['rho_int'], file['isopl']
-k_a = 20
-
-# (b) Diff R (Ekman vs w) - (wg vs w) sigma 26
-file1 = sio.loadmat('.../OCCITENS/rho_wek_w_occitens_glob_025_annual_isolevm_filtr.mat')
-file2 = sio.loadmat('.../OCCITENS/rho_w_wglvb_occitens_glob_025_annual_isolevm_filtr.mat')
-rho_b1, rho_b2 = file1['rho_int'], file2['rho_int']
-lon, lat = file1['LONimone'], file1['LATimone']
-k_b = 20
-diff_b = np.clip(rho_b1[:, :,k_b] - rho_b2[:, :,k_b], -1, 1)
-
-# (c) R (wg vs w) 100 m
-file = sio.loadmat('.../OCCITENS/rho_wglvb_w_occitens_glob_025_annual_horlev_filtr.mat')
-rho_c = file['rho_int'][:, :]
-
-# (d) R (Ekman vs w) 100 m
-file = sio.loadmat('.../OCCITENS/rho_wek_w_occitens_glob_025_annual_horlev_filtr.mat')
-rho_d = file['rho_int'][:, :]
-
-# Figure:
-fig, axs             = plt.subplots(2, 2, figsize=(22, 12), subplot_kw={"projection": proj})
-axs                  = axs.ravel()
-
-datasets             = [rho_a[:, :, k_a],diff_b,rho_c,rho_d]
-highlight_levels_all = [[0.7, 0.9],[0],[0.7, 0.9],[0.7, 0.9]]
-linestyles_all       = [['--', '-'],['-'],['--', '-'],['--', '-']]
-
-for i, ax in enumerate(axs):
-    pcm = ax.contourf(lon, lat, datasets[i], levels=bounds, cmap=BrBGcentre, norm=norm,
-                      transform=ccrs.PlateCarree(), zorder=10)
-    
-    hc = ax.contour(lon, lat, datasets[i], levels=highlight_levels_all[i],
-                    colors='black', linewidths=1.5, linestyles=linestyles_all[i],
-                    transform=ccrs.PlateCarree(), zorder=50)
-    
-    ax.add_feature(cfeature.NaturalEarthFeature(
-        'physical', 'land', '50m', edgecolor='none', facecolor='k', linewidth=0.25), zorder=100)
-    
-    gl = ax.gridlines(draw_labels=True, xlocs=range(-180, 181, 90), ylocs=range(-60, 61, 30), color='gray', zorder=1000)
-    gl.top_labels = False
-    gl.right_labels = False
-    gl.xlabel_style = {'size': 20}
-    gl.ylabel_style = {'size': 20}
-    
-    for spine in ax.spines.values():
-        spine.set_linewidth(2)
-    
-    ax.set_title(titles[i], fontsize = 24)
-
-# Colorbar
-cbar_ax = fig.add_axes([0.92, 0.2, 0.015, 0.6])
-cbar = fig.colorbar(mpl.cm.ScalarMappable(cmap=BrBGcentre, norm=norm), cax=cbar_ax,
-                    orientation='vertical', ticks=boundstick, extend='both')
-cbar.ax.tick_params(labelsize=20)
-cbar.set_label('Correlation coefficient', size=22)
-
-plt.subplots_adjust(wspace=0.1, hspace=0.2)
-
-plt.savefig('figure9_VF.png', bbox_inches='tight', dpi=300)
-
-
