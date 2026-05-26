47ea649bc176a38d14e3cb1208e8b1c094620bbd
Diego Cortes-Morales <47925616+dcortales@users.noreply.github.com>
2026-05-26 17:20:16 +0200

    Refactor file paths and save figure as figure3.png
    Updated file paths to relative paths for portability and modified figure saving path.

--- figures/figure_3.py
+++ figures/figure_3.py
@@ -1,3 +1,4 @@
+# FIGURE 3: Vertical profiles during 1993-2015 period in the North Atlantic Ocean
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib as mpl
@@ -11,17 +12,17 @@ import xarray as xr
 # %% Load variables
 
 # MLD
-MLDm            = sio.loadmat('C:\\Users\\yago_\\Documents\\LOCEAN\\OLIV3 paper\\MLD_ARMOR3D_GLOB.mat')
+MLDm            = sio.loadmat('...\MLD_ARMOR3D_GLOB.mat')
 mldcont         = MLDm['mld_contac']
 
 # Isopycnal level depth:
-filo            = 'C:\\Users\yago_\Documents\LOCEAN\Data\OLIV3\oliv3_glob_025_ekf_annual_isolevm.nc'
+filo            = '...\Data\OLIV3\oliv3_glob_025_ekf_annual_isolevm.nc'
 file            = xr.open_dataset(filo)
 h_isolev        = file.variables['h_isolev'].values.T
 h_isolev        = h_isolev[:,:,:,0]
 
 # OLIV3 vertical velocities and 3d grid
-filo            = 'C:\\Users\yago_\Documents\LOCEAN\Data\OLIV3\oliv3_glob_025_ekf_annual_isolevm_5filtr.nc'
+filo            = '...\Data\OLIV3\oliv3_glob_025_ekf_annual_isolevm_5filtr.nc'
 file            = xr.open_dataset(filo)
 lon, lat, iso   = file.variables['longitude'].values.T, file.variables['latitude'].values.T, file.variables['isolev'].values.T
 w               = np.mean(file.variables['w_oliv3_isolevf'].values.T,3)
@@ -29,7 +30,7 @@ w               = np.mean(file.variables['w_oliv3_isolevf'].values.T,3)
 dimlon, dimlat, dimdep = lon.shape[0], lat.shape[1], len(iso)
 
 # Ekman pumping
-filo            = 'C:\\Users\yago_\Documents\LOCEAN\Data\ERA5\ekman_pumping_glob_025_filtered_annual.nc'
+filo            = '...\Data\ERA5\ekman_pumping_glob_025_filtered_annual.nc'
 file            = xr.open_dataset(filo)
 lone, late      = file.variables['longitude'].values.T, file.variables['latitude'].values.T
 wek             = np.mean(file.variables['wek_era5'].values.T,2)
@@ -44,17 +45,6 @@ RBens1      = RBens1m['RBens1']
 cmp1        = ListedColormap(RBens1)
 
 anno = np.arange(1993,2016)
-# %% Hovmoller diagram OLIV3 + Ekman pumping ---------------------------------
-
-filo            = 'C:\\Users\yago_\Documents\LOCEAN\Data\OLIV3\oliv3_glob_025_ekf_annual_isolevm_5filtr.nc'
-file            = xr.open_dataset(filo)
-
-filok            = 'C:\\Users\yago_\Documents\LOCEAN\Data\ERA5\ekman_pumping_glob_025_filtered_annual.nc'
-filek            = xr.open_dataset(filok)
-lone, late      = filek.variables['longitude'].values.T, filek.variables['latitude'].values.T
-wek             = filek.variables['wek_era5'].values.T
-
-wek[abs(late)<5] = np.nan
 
 # Regionalisation:
 
@@ -154,16 +144,13 @@ for t in np.arange(0,len(anno)):
 
 w_isopp[35,:] = wekp
 
-## FIGURE
-
-scale       = 10**(-6)
- 
+# Figure
+scale = 10**(-6)
 vmin = -1.5e-6
 vmax = 1.5e-6
 cmp = 'RdBu_r'
 
 # Create subplots
-
 scale       = 10**(-6)
 contens     = [-200, -10, -8, -6, -4, -2, -1, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1, 2, 4, 6, 8, 10, 200]
 contens2    = np.multiply(contens,scale)
@@ -177,10 +164,9 @@ norm        = mpl.colors.BoundaryNorm(bounds2, cmp1.N)
 vmin = -0.12
 vmax = 0.12
 
-
 fig, axs = plt.subplots(3, 1, figsize=(7, 12))
 
-# Panel 1: Original variable with pcolormesh
+# Panel 1:
 pc0 = axs[0].pcolor(anno, iso[14:22], w_isopt[14:22,:]*60*60*24, cmap=cmp, vmin=vmin, vmax=vmax)
 contours = axs[0].contour(anno, iso[13:23], w_isopt[13:23,:]*60*60*24, colors='black', vmin=vmin/scale, vmax=vmax/scale)
 plt.clabel(contours, inline=True, fontsize=8)
@@ -188,12 +174,11 @@ axs[0].invert_yaxis()
 axs[0].set_title('(a) NATL Tropical gyre [30-16$^o$W]:[8-16$^o$N]',fontsize=16)
 axs[0].set_ylabel('Isopycnal level (kg/m$^3$)',fontsize=14)
 axs[0].set_xticklabels([])
-#axs[0,0].hlines((iso[13]+iso[14])/2,1992,2020,color='black')
 axs[0].set_xlim([1992.5,2015.5])
 axs[0].set_ylim([(iso[21]+iso[22])/2,(iso[13]+iso[14])/2])
 
 
-# Panel 2: First additional variable
+# Panel 2: 
 pc1 = axs[1].pcolormesh(anno, iso[19:29], w_isops[19:29,:]*60*60*24, cmap=cmp, vmin=vmin, vmax=vmax)
 contours = axs[1].contour(anno, iso[18:30], w_isops[18:30,:]*60*60*24, colors='black', vmin=vmin/scale, vmax=vmax/scale)
 plt.clabel(contours, inline=True, fontsize=8)
@@ -201,11 +186,10 @@ axs[1].invert_yaxis()
 axs[1].set_title('(b) NATL Subtropical gyre [50-30$^o$W]:[15-30$^o$N]',fontsize=16)
 axs[1].set_ylabel('Isopycnal level (kg/m$^3$)',fontsize=14)
 axs[1].set_xticklabels([])
-#axs[1,0].hlines((iso[18]+iso[19])/2,1992,2020,color='black')
 axs[1].set_xlim([1992.5,2015.5])
 axs[1].set_ylim([(iso[28]+iso[29])/2,(iso[18]+iso[19])/2])
 
-# Panel 3: Second additional variable
+# Panel 3:
 pc2 = axs[2].pcolormesh(anno, iso[36:55], w_isopp[36:55,:]*60*60*24, cmap=cmp, vmin=vmin, vmax=vmax)
 contours = plt.contour(anno, iso[35:56], w_isopp[35:56,:]*60*60*24, colors='black', vmin=vmin/scale, vmax=vmax/scale)
 plt.clabel(contours, inline=True, fontsize=8)
@@ -213,8 +197,6 @@ axs[2].invert_yaxis()
 axs[2].set_title('(c) NATL Subpolar gyre [55-30$^o$W]:[50-60$^o$N]',fontsize=16)
 axs[2].set_ylabel('Isopycnal level (kg/m$^3$)',fontsize=14)
 axs[2].set_xlabel('Years',fontsize=14)
-#axs[2,0].hlines((iso[35]+iso[36])/2,1992,2020,color='black')
-#fig.colorbar(pc2, ax=axs[2,0], orientation='vertical', label='w [m/s]')
 axs[2].set_xlim([1992.5,2015.5])
 axs[2].set_ylim([(iso[54]+iso[55])/2,(iso[35]+iso[36])/2])
 
@@ -226,12 +208,12 @@ for ax in axs:
 pos = axs[2].get_position()
 cax = fig.add_axes([pos.x0, 0.95, pos.width, 0.015])
 scale       = 10**(-7)
-# contens_ticks     = np.arange(-15,17.5,2.5)*scale
+
 contens_ticks     = np.arange(-0.12,0.146,0.04)
 boundticks     = np.round(np.arange(-0.12,0.146,0.04),2)
-# boundticks  = np.round(np.arange(-1.5,1.75,0.25),2)
+
 cb = fig.colorbar(pc2,cax=cax, orientation='horizontal',extend='both',ticks=contens_ticks)
 cb.set_label('Vertical velocity ($m$ $day^{-1}$)', labelpad=-60, fontsize=14, loc='center')
 cb.ax.set_xticklabels(boundticks,fontsize=12)
 
-#plt.savefig('C:\\Users\\yago_\\Documents\\LOCEAN\\OLIV3 paper\\V4 codes\\figuresVF\\figure2_VF.png',bbox_inches='tight', dpi=300)
+plt.savefig('...\figures\figure3.png',bbox_inches='tight', dpi=300)
